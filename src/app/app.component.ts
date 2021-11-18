92aa412a948c0af7e2b6c27bb2fddb511012c8ec
Kapil Kahar <kaharkapil@gmail.com>
2021-11-18 22:17:52 +0530

    basic frontend

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'url-assignment-frontend';
+
+
+
+  shortURL: any = "shortURL"
+
+  expiresIn: any = "1/21/2021"
+
+}
