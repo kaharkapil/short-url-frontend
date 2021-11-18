6e1cb41d64be4e47c12fa5c303d9a9dfe2557369
Kapil Kahar <kaharkapil@gmail.com>
2021-11-19 04:15:33 +0530

    final submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,11 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'url-assignment-frontend';
+  constructor(private httpClient: HttpClient) { }
+  shortURL;
 
+  expiresIn;
+  longURL;
 
-
-  shortURL: any = "shortURL"
-
-  expiresIn: any = "1/21/2021"
-
+  submit() {
+    console.log(this.longURL);
+    let data = {
+      url: this.longURL
+    }
+    this.httpClient.post(`http://localhost:3000/generate`, data).subscribe(
+      (response: any) => {
+        console.log(response.url);
+        this.shortURL = response.url
+        this.expiresIn = response.expiresIn
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
